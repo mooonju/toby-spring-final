7b6c74439b016f2953b6931883dc5bd0ee5c62c0
MUNJU <mvv0030@gmail.com>
2022-10-25 13:59:40 +0900

    [Spring] ConnectionMaker Class 분리

--- src/main/java/com/likelion/dao/AwsConnectionMaker.java
+++ src/main/java/com/likelion/dao/AwsConnectionMaker.java
@@ -7,7 +7,7 @@ import java.util.Map;
 
 public class AwsConnectionMaker implements ConnectionMaker{
     @Override
-    public Connection makeConnection() throws ClassNotFoundException, SQLException {
+    public Connection makeConnection() throws SQLException {
         Map<String, String> env = System.getenv();
         Connection c = DriverManager.getConnection(env.get("DB_HOST"),
                 env.get("DB_USER"), env.get("DB_PASSWORD"));
