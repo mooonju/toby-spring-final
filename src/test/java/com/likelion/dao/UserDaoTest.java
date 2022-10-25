9901f4d8793c730e0c20bb640e916e8e06237cf9
MUNJU <mvv0030@gmail.com>
2022-10-25 13:28:37 +0900

    [Spring] deleteAll getCount 테스트

--- src/test/java/com/likelion/dao/UserDaoTest.java
+++ src/test/java/com/likelion/dao/UserDaoTest.java
@@ -26,11 +26,35 @@ class UserDaoTest {
     @DisplayName("insert select test")
     void addAndGet() throws SQLException, ClassNotFoundException {
         UserDao userDao = context.getBean("awsUserDao", UserDao.class);
+        userDao.deleteAll();
+        assertEquals(0, userDao.getCount());
+
         String id = "11";
         userDao.add(new User(id, "Roro", "1312"));
+        assertEquals(1, userDao.getCount());
         User user = userDao.findById(id);
+
         assertEquals("Roro", user.getName());
         assertEquals("1312", user.getPassword());
+    }
+
+    @Test
+    @DisplayName("count")
+    void count() throws SQLException, ClassNotFoundException {
+        User user1 = new User("1", "AAA", "1111");
+        User user2 = new User("2", "BBB", "2222");
+        User user3 = new User("3", "CCC", "3333");
+        UserDao userDao = context.getBean("awsUserDao", UserDao.class);
+
+        userDao.deleteAll();
+        assertEquals(0, userDao.getCount());
+
+        userDao.add(user1);
+        assertEquals(1, userDao.getCount());
+        userDao.add(user2);
+        assertEquals(2, userDao.getCount());
+        userDao.add(user3);
+        assertEquals(3, userDao.getCount());
     }
 
 }
