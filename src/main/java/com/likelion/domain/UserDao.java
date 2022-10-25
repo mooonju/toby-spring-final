73e68244c04b8a407a76468c09a2e39ffdc401f8
MUNJU <mvv0030@gmail.com>
2022-10-25 11:18:55 +0900

    Spring 복습

---
+++ src/main/java/com/likelion/domain/UserDao.java
@@ -0,0 +1,57 @@
+package com.likelion.domain;
+
+import com.likelion.dao.User;
+
+import java.sql.*;
+import java.util.Map;
+
+public class UserDao {
+    public void add() throws ClassNotFoundException, SQLException {
+        Map<String, String> env = System.getenv();
+        String dbHost = env.get("DB_HOST");
+        String dbUser = env.get("DB_USER");
+        String dbPassword = env.get("DB_PASSWORD");
+
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        Connection c = DriverManager.getConnection(dbHost, dbUser, dbPassword);
+        PreparedStatement ps = c.prepareStatement("insert into users(id, name, password) " +
+                "values(?, ?, ?)");
+        ps.setString(1, "01");
+        ps.setString(2, "AAA");
+        ps.setString(3, "password");
+
+        ps.executeUpdate();
+
+        ps.close();
+        c.close();
+    }
+
+    public User findById(String id) throws ClassNotFoundException, SQLException {
+        Map<String, String> env = System.getenv();
+        String dbHost = env.get("DB_HOST");
+        String dbUser = env.get("DB_USER");
+        String dbPassword = env.get("DB_PASSWORD");
+
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        Connection c = DriverManager.getConnection(dbHost, dbUser, dbPassword);
+        PreparedStatement ps = c.prepareStatement("select * from users where id = ?");
+
+        ps.setString(1, id);
+
+        ResultSet rs = ps.executeQuery();
+        rs.next();
+        User user = new User(rs.getString("id"),
+                rs.getString("name"), rs.getString("password"));
+
+        rs.close();
+        ps.close();
+        c.close();
+
+        return user;
+    }
+
+    public static void main(String[] args) throws SQLException, ClassNotFoundException {
+        UserDao dao = new UserDao();
+        dao.add();
+    }
+}
